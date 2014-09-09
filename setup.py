2c1836eff605b0faffcfcfffc5e5e40fff037f65
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2014-09-09 16:07:57 +0200

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ See https://github.com/eandersson/amqp-storm for more information.
 """
 
 setup(name='AMQP-Storm',
-      version='1.1.0a1',
+      version='1.1.0',
       description='Thread-safe Python AMQP Client Library based on pamqp.',
       long_description=long_description,
       author='Erik Olof Gunnar Andersson',
