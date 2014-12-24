ed769ddc3b6f8328d77e1cdc3d1e16af5d6910b5
Erik Olof Gunnar Andersson <me@eandersson.net>
2014-12-24 15:06:38 +0100

    Fixed incorrect python.py version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ See https://github.com/eandersson/amqp-storm for more information.
 """
 
 setup(name='AMQP-Storm',
-      version='1.1.5',
+      version='1.1.6',
       description='Thread-safe Python AMQP Client Library based on pamqp.',
       long_description=long_description,
       author='Erik Olof Gunnar Andersson',
