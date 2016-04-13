9834978032b74198dbc7d9b65c6d26bc49cd3326
Erik Olof Gunnar Andersson <eandersson@users.noreply.github.com>
2016-04-13 16:37:34 +0200

    Fixed a mistake in the generator example

--- examples/simple_generator_consumer.py
+++ examples/simple_generator_consumer.py
@@ -9,7 +9,7 @@ def consumer():
     with Connection('127.0.0.1', 'guest', 'guest') as connection:
         with connection.channel() as channel:
             channel.queue.declare('simple_queue')
-            channel.basic.consume('simple_queue', no_ack=False)
+            channel.basic.consume(queue='simple_queue', no_ack=False)
             for message in channel.build_inbound_messages():
                 print(message.body)
                 message.ack()
