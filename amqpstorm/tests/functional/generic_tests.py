6fe30b95bfde4f11ac2a61dd793aad9f080862b3
Erik Olof Gunnar Andersson <me@eandersson.net>
2016-06-08 23:25:59 +0200

    Adding unit-test for bug (#16) and temporarily disabling pypy3 tests

--- amqpstorm/tests/functional/generic_tests.py
+++ amqpstorm/tests/functional/generic_tests.py
@@ -183,6 +183,38 @@ class PublishLargeMessagesAndConsumeTest(unittest.TestCase):
         self.connection.close()
 
 
+class PublishEmptyMessagesAndConsumeTest(unittest.TestCase):
+    def setUp(self):
+        self.queue_name = 'test.basic.empty_messages'
+        self.connection = Connection(HOST, USERNAME, PASSWORD)
+        self.channel = self.connection.channel()
+        self.channel.confirm_deliveries()
+        self.channel.queue.declare(self.queue_name)
+        self.channel.queue.purge(self.queue_name)
+
+    def test_functional_publish_5_empty_messages(self):
+        body = b''
+        messages_to_publish = 5
+
+        self.channel.basic.consume(queue=self.queue_name,
+                                   no_ack=True)
+        # Publish 5 Messages.
+        for _ in range(messages_to_publish):
+            self.channel.basic.publish(body=body,
+                                       routing_key=self.queue_name)
+
+        inbound_messages = []
+        for message in self.channel.build_inbound_messages(break_on_empty=True):
+            self.assertEqual(message.body, body)
+            inbound_messages.append(message)
+        self.assertEqual(len(inbound_messages), messages_to_publish)
+
+    def tearDown(self):
+        self.channel.queue.delete(self.queue_name)
+        self.channel.close()
+        self.connection.close()
+
+
 class PublishLargeMessagesAndGetTest(unittest.TestCase):
     def setUp(self):
         self.connection = Connection(HOST, USERNAME, PASSWORD)
