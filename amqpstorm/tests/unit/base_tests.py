2bc47d62c152f704cc4f6e81b3ccadd37788e210
Erik Olof Gunnar Andersson <me@eandersson.net>
2016-11-13 00:31:25 -0800

    Updated BaseChannel code and unit-tests

--- amqpstorm/tests/unit/base_tests.py
+++ amqpstorm/tests/unit/base_tests.py
@@ -7,6 +7,7 @@ except ImportError:
 
 from amqpstorm.base import Stateful
 from amqpstorm.base import BaseChannel
+from amqpstorm.exception import AMQPChannelError
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,24 +20,49 @@ class BasicChannelTests(unittest.TestCase):
 
     def test_base_channel_add_consumer_tag(self):
         channel = BaseChannel(0)
-        channel.add_consumer_tag('my_tag')
+        channel.add_consumer_tag('travis-ci')
 
-        self.assertEqual(channel.consumer_tags[0], 'my_tag')
+        self.assertEqual(channel.consumer_tags[0], 'travis-ci')
+
+    def test_base_channel_add_consumer_tag_none_raises(self):
+        channel = BaseChannel(0)
+        self.assertRaisesRegexp(AMQPChannelError,
+                                'consumer tag needs to be a string',
+                                channel.add_consumer_tag, None)
+        self.assertFalse(channel.consumer_tags)
+
+    def test_base_channel_remove_empty_string(self):
+        channel = BaseChannel(0)
+        channel.add_consumer_tag('travis-ci')
+        channel.add_consumer_tag('')
+
+        channel.remove_consumer_tag('')
+
+        self.assertTrue(len(channel.consumer_tags))
+
+    def test_base_channel_add_duplicate_consumer_tags(self):
+        channel = BaseChannel(0)
+
+        channel.add_consumer_tag('travis-ci')
+        channel.add_consumer_tag('travis-ci')
+
+        self.assertEqual(len(channel.consumer_tags), 1)
+        self.assertEqual(channel.consumer_tags[0], 'travis-ci')
 
     def test_base_channel_remove_single_consumer_tag(self):
         channel = BaseChannel(0)
 
         self.assertEqual(len(channel.consumer_tags), 0)
 
-        channel.add_consumer_tag('1')
-        channel.add_consumer_tag('2')
+        channel.add_consumer_tag('travis-ci-1')
+        channel.add_consumer_tag('travis-ci-2')
 
         self.assertEqual(len(channel.consumer_tags), 2)
 
-        channel.remove_consumer_tag('1')
+        channel.remove_consumer_tag('travis-ci-1')
 
         self.assertEqual(len(channel.consumer_tags), 1)
-        self.assertEqual(channel.consumer_tags[0], '2')
+        self.assertEqual(channel.consumer_tags[0], 'travis-ci-2')
 
     def test_base_channel_remove_all_consumer_tags(self):
         channel = BaseChannel(0)
@@ -44,8 +70,8 @@ class BasicChannelTests(unittest.TestCase):
         self.assertEqual(len(channel.consumer_tags), 0)
 
         for _ in range(100):
-            channel.add_consumer_tag('my_tag')
-        channel.remove_consumer_tag()
+            channel.add_consumer_tag('travis-ci')
+        channel.remove_consumer_tag(None)
 
         self.assertEqual(len(channel.consumer_tags), 0)
 
