09d4f128c15b175a048504dba586ae93e79bd990
Erik Olof Gunnar Andersson <me@eandersson.net>
2018-04-20 01:48:15 -0700

    Varius functional testing improvements

--- amqpstorm/tests/__init__.py
+++ amqpstorm/tests/__init__.py
@@ -1,5 +1,7 @@
-HOST = '127.0.0.1'
-USERNAME = 'guest'
-PASSWORD = 'guest'
-URI = 'amqp://guest:guest@127.0.0.1:5672/%2F'
-HTTP_URL = 'http://127.0.0.1:15672'
+import os
+
+HOST = os.environ.get('AMQP_HOST', '127.0.0.1')
+USERNAME = os.environ.get('AMQP_USERNAME', 'guest')
+PASSWORD = os.environ.get('AMQP_PASSWORD', 'guest')
+URI = os.environ.get('AMQP_URI', 'amqp://guest:guest@127.0.0.1:5672/%2F')
+HTTP_URL = os.environ.get('AMQP_HTTP_URL', 'http://127.0.0.1:15672')
