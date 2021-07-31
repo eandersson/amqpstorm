1da5a3e08eb2357b0bf55663938158677a51d705
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2021-07-31 15:15:45 -0700

    Specify python_requires even for the 2.x build

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ def get_version(rel_path):
 
 setup(
     name='AMQPStorm',
+    python_requires='>=2.7',
     version=get_version('amqpstorm/__init__.py'),
     description='Thread-safe Python RabbitMQ Client & Management library.',
     long_description=open('README.rst').read(),
