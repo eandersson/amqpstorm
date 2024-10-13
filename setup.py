b566864c4b3163aa1f4fbb483c5374e58bc02856
Erik Olof Gunnar Andersson <me@eandersson.net>
2024-10-13 15:16:07 +0200

    Update url

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     include_package_data=True,
     packages=find_packages(),
     license='MIT License',
-    url='https://www.amqpstorm.io',
+    url='https://github.com/eandersson/amqpstorm',
     install_requires=['pamqp>=2.0.0,<3.0'],
     extras_require={
         'management': ['requests>2'],
